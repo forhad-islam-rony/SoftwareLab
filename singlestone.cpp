85e971de2ab662ac12ed925a98be6b5bbc7c0919
Forhad Islam Rony <forhadrony161@gmail.com>
2024-01-22 16:01:15 +0600

    Shape Square Rectangular class created

--- singlestone.cpp
+++ singlestone.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Shape{
+    public:
+    virtual void draw()=0;
+};
+class Rectangle : public Shape{
+    public:
+    void draw() override{
+     cout<<"Inside rectangle :: draw() method."<<endl;
+    }
+};
+
+class Square : public Shape{
+    public:
+    void draw() override{
+        cout<<"Inside square::draw() method."<<endl;
+    }
+};
+
+
 int main(){
-    
+
 }
